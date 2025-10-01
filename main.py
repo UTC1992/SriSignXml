70d32aaba8c98d794c324a9416a4c6f47b4c3c9b
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2025-10-01 09:41:48 -0500

    config docker compose and improve elements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from app.routes.invoice import routerInvoice
+from app.routes.invoice import router_invoice
 from fastapi import FastAPI
 
 app = FastAPI()
 
-app.include_router(router=routerInvoice)
+app.include_router(router=router_invoice)
